91f241f1f0063d4b7cd73ef8c9b963506d3397c4
Shibata Hironori <46587015+hilosiva@users.noreply.github.com>
2023-11-19 15:00:17 +0900

    UPDATE: コンソールへの表記を追加

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,4 @@
+/* --------- 　ここから編集禁止  ------------- */
+console.info("\n %c Orelop Static - https://github.com/hilosiva/orelop-static \n", "color: #66ffa5; background: #001010; padding:8px 0;");
 import.meta.glob(["../img/**"]);
-
-console.log("Ready Orelop");
+/* --------- 　ここまで編集禁止  ------------- */
