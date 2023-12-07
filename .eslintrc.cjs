3091a08a0e7eaca2c50a6d29d87a31e9b174a2f2
Shibata Hironori <46587015+hilosiva@users.noreply.github.com>
2023-12-08 01:04:50 +0900

    add: ESLintの導入

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:tailwindcss/recommended", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+};
